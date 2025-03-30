001211434d55c6b48294a564be3eea25c16c7735
Mariana Abreu <marianalltabreu@gmail.com>
2025-03-30 23:54:45 +0100

    Convert table to dict updates

--- processing/process_json_data.py
+++ processing/process_json_data.py
@@ -29,7 +29,7 @@ def extract_tables(data, mode='all', heading=''):
 def process_table(data, heading):
     """Process one table from the json, the one that is after the heading
     """
-    with open('/app/code/processing/field_mappings.yaml', 'r') as file:
+    with open('processing/field_mappings.yaml', 'r') as file:
         field_mappings = yaml.safe_load(file)['field_mappings']
     map_list = field_mappings[heading]
     for map in map_list:
@@ -37,19 +37,24 @@ def process_table(data, heading):
         if table != -1:
             break
     if table == -1:
+        return []
+    else: 
+        return table
+
+def convert_dict(table):
+    # table is a list
+    # pass first 
+    if table == []:
         return {}
     cols = [col if col != '' else 'Column ' + str(i) for i, col in enumerate(table[0]) ]
     table = pd.DataFrame(table[1:], columns=cols)
     if table.shape[1] == 2:
         table_dict = dict(zip(table.iloc[:,0].values, table.iloc[:,1]))
     else:
-        if heading in ['Maturity Breakdown']:
-            values = (table.iloc[:,1::2].T.values).reshape(-1)
-            keys = (table.iloc[:,::2].T.values).reshape(-1)
-            table_dict = dict(zip(keys, values))
-        else:
-            print('This table requires more processing')
-            table_dict = {}
+        values = (table.iloc[:,1::2].T.values).reshape(-1)
+        keys = (table.iloc[:,::2].T.values).reshape(-1)
+        table_dict = dict(zip(keys, values))
+    
     return table_dict
 
     
@@ -68,72 +73,53 @@ def extract_year_performance(json_file_path):
     field = "12-month Performance"
     data = load_json(json_file_path)
     json_table = process_table(data, field)
-    return json_table
+    return convert_dict(json_table)
 
 def extract_market_allocation(json_file_path):
     field = "Market Allocation"
     data = load_json(json_file_path)
     json_table = process_table(data, field)
-    return json_table
+    return convert_dict(json_table)
 
 
 def extract_credit_rate(json_file_path):
     field = "Credit Rating"
     data = load_json(json_file_path)
     json_table = process_table(data, field)
-
-    return json_table
+    return convert_dict(json_table)
 
 def extract_sector(json_file_path):
     field = "Sector Breakdown"
     data = load_json(json_file_path)
     json_table = process_table(data, field)
-
-    return json_table
+    return convert_dict(json_table)
 
 def extract_annualised_performance(json_file_path):
     field = "Annualised Performance"
     data = load_json(json_file_path)
     json_table = process_table(data, field)
-
-    return json_table
-
-
-def convert_dict(input_dict):
-
-    print(input_dict)
-
-    # Extract the values from the input dictionary
-    keys = input_dict["Column 0"]
-    values = input_dict["Column 1"]
-    
-    # Create a new dictionary to hold the result
-    result = {}
-    
-    # Iterate through the keys and values to populate the result dictionary
-    for i in range(len(keys)):
-        result[keys[i]] = values[i]
-    
-    return result
+    return convert_dict(json_table)
 
 
 def extract_maturity(json_file_path:str):
     field = 'Maturity Breakdown'
     data = load_json(json_file_path)
     json_table = process_table(data, field)
-    # return convert_dict(json_table)
-    return json_table
+    return convert_dict(json_table)
+
 
 if __name__ == '__main__':
-    print("Here")
+
     json_dir = os.path.join(os.getcwd(), 'files', 'json')
-    df_json = pd.DataFrame()
-    field = "Annualised performance"
     for file in os.listdir(json_dir):
+        
         json_file = os.path.join(json_dir, file)
         data = load_json(json_file)
+        json_table = extract_maturity(json_file)
+        print(json_table)
+
         json_table = extract_credit_rate(json_file)
-        json_table = extract_annualised_performance(json_file)
-        json_table = extract_year_performance(json_file)
+        print(json_table)
+        
         json_table = extract_sector(json_file)
         print(json_table)
